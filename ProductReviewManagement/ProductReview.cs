086dea4863383591e2f61d7065c7ee92b7a66c0e
Amartya <amartyaanand21@live.com>
2020-11-06 23:26:06 +0530

    [Amartya] Refactor : Adding missing details of Product Review Class.

--- ProductReviewManagement/ProductReview.cs
+++ ProductReviewManagement/ProductReview.cs
@@ -6,5 +6,10 @@ namespace ProductReviewManagement
 {
     class ProductReview
     {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
     }
 }
